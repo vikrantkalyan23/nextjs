8b6dca71c36eb402f5e0720dddcc8b4013d510ca
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-04-20 14:15:22 +0530

    #navigation and routes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
 const About = () => {
+  const route = useRouter();
+  const navigate = (url) => {
+    route.push(url);
+  }
   return (
     <div>
       <h2>About</h2>
+      <button onClick={()=>navigate('/')}>Home</button>
     </div>
   );
 };
